5c780f7397ea249404eda31a6f12577f0343855e
ALVIN2759722771 <142221446+ALVIN2759722771@users.noreply.github.com>
2023-08-16 21:50:51 +0530

    Update contact_form.js

--- contact_form.js
+++ contact_form.js
@@ -1,22 +1,31 @@
 document.getElementById("contactForm").addEventListener("submit", function(event) {
   event.preventDefault();
 
+  const fromName = document.getElementById("from_name").value;
+  const recipientName = document.getElementById("recipient_name").value; // Capture recipient's name
+  const userEmail = document.getElementById("email").value;
+  const emailSubject = document.getElementById("subject").value;
+  const userMessage = document.getElementById("message").value;
+
   const emailParams = {
-    to_name: "Recipient Name",
-    from_name: document.getElementById("email").value,
-    subject: document.getElementById("subject").value,
-    message: document.getElementById("message").value
+    to_name: recipientName, // Use recipient's name from the form
+    from_name: fromName,
+    subject: emailSubject,
+    message: userMessage
   };
 
-  // Use EmailJS or another method to send the email
   emailjs.send("default_service", "template_6le5ttk", emailParams)
     .then(function(response) {
       alert("Email sent successfully!");
       // Clear form fields
+      document.getElementById("from_name").value = "";
+      document.getElementById("recipient_name").value = ""; // Clear recipient's name field
       document.getElementById("email").value = "";
       document.getElementById("subject").value = "";
       document.getElementById("message").value = "";
-    }, function(error) {
+    })
+    .catch(function(error) {
       alert("Failed to send email. Please try again later.");
+      console.error("Email Error:", error);
     });
 });
